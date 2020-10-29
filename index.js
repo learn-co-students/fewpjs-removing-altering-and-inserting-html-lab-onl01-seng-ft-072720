d89dfad43491e994783ab58b07331b7027e7032d
Your Username <you@example.com>
2020-10-29 15:47:14 -0600

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ document.getElementById('main').remove();
 var newHeader = document.createElement('h1');
 newHeader.setAttribute("id","victory");
 newHeader.innerHTML = "KYLE is the champion";
-// innerText = document.createTextNode("LUKAAAAA is the champion");
-// newHeader.appendChild(innerText);
 document.body.appendChild(newHeader);
