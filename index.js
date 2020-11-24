0b1163b461e9deb16bdb116d72f70b49bce85e24
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-11-24 23:13:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-let main = document.getElementById("main");
-main.remove();
-
-let newHeader = document.createElement("h1");
+document.querySelector("main#main").remove();
+let newHeader = document.createElement('h1')
 newHeader.id = "victory";
-newHeader.innerHTML = 'is the champion';
+newHeader.innerHTML = "YOUR-Name is the champion";
